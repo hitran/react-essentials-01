fddb610dadade5f1461e0beafa324d0f2a3f3dad
thuc_hi_tran <thuchitran@gmail.com>
2025-02-23 16:00:40 +1100

    add unit tests

---
+++ with_testing/src/components/CoreConcept.tsx
@@ -0,0 +1,17 @@
+type CoreConceptProps = {
+  image: string;
+  title: string;
+  description: string;
+};
+
+function CoreConcept({ image, title, description }: CoreConceptProps) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+export default CoreConcept;
